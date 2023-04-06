d43bbdb8764a24b5fe76f04a17f3f32f45348306
Muhammad Daniyal <mr.dannii98@gmail.com>
2023-04-06 20:31:03 +0500

    Updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,6 +42,14 @@
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            
+        </div>
+
+        <div class="flex items-center justify-start mt-4">
+                <a class=" text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                    {{ __("Don't have an Account") }}
+                </a>
+            
         </div>
     </form>
 </x-guest-layout>
