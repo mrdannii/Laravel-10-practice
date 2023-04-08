501f8729b68fcce4c0f83a8892fa40b9edca4e6d
Muhammad Daniyal <mr.dannii98@gmail.com>
2023-04-08 21:42:51 +0500

    login in with github

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+use OpenAI\Laravel\Facades\OpenAI;
+
+
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request)
+    {
+       if($oldavatar = $request->user()->avatar)
+       {
+            Storage::disk('public')->delete($oldavatar);
+       }
+            $path = Storage::disk('public')->put('avatars',$request->file('avatar'));
+       //  $path = $request->file('avatar')->store('avatars','public');
+       Auth::user()->update(['avatar'=>$path]);
+        return redirect('/profile')->with('status', 'avatar-updated');
+    }
+
+    public function generate(Request $request)
+    {
+       
+       
+        $result = OpenAI::images()->create([
+            "prompt"=> "create avatar for user Profile animated related to tech",
+          "n"=> 1,
+          "size"=> "256x256"
+        ]);
+
+        if($oldavatar = $request->user()->avatar)
+        {
+             Storage::disk('public')->delete($oldavatar);
+        }
+        $content=file_get_contents($result->data[0]->url);
+        $filename= Str::random(25);
+        Storage::disk('public')->put("avatars/$filename.jpg",$content);
+       
+
+        Auth::user()->update(['avatar'=>"avatars/$filename.jpg"]);
+
+
+        return redirect('/profile')->with('status', 'avatar-generated');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Profile\AvatarController;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+use OpenAI\Laravel\Facades\OpenAI;
+use Laravel\Socialite\Facades\Socialite;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +23,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // $a=User::where('id',2)->first();
+    // dd($a);
     return view('welcome');
 });
 
@@ -26,6 +36,39 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
+
+    Route::post('/profile/avatar/ai', [AvatarController::class, 'generate'])->name('profile.avatar.ai');
+
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+}) ->name('github.login');
+
+Route::get('/auth/callback', function () {
+    $githubUser = Socialite::driver('github')->user();
+
+    if($githubUser->name == NULL)
+    {
+        $xname= Str::random(7);
+    }
+    else{
+        $xname= $githubUser->name;
+    }
+
+    $user = User::firstOrCreate(['email' => $githubUser->email, ],[
+    'name' => $xname,
+     'password' => 'password',
+ ]);
+
+    Auth::login($user);
+    return redirect('/dashboard');
+
+    //dd($githubUser);
+    // $user->token
+});
